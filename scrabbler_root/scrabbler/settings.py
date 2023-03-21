5327535de451df9d15a649bc3d1bad7daa4cf1c0
Ciaran Brennan <cjbrennan2701@gmail.com>
2023-03-21 14:51:40 +0000

    Updated settings

--- scrabbler_root/scrabbler/settings.py
+++ scrabbler_root/scrabbler/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's%3-f0&r!b14gt(i=hzfxa8skg(u#sq9v&8!r+1!7*dvw-uk^p'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
